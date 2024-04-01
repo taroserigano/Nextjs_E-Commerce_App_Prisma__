8fc72d5dfd7a75e8582c2902b115d47ef4f35032
Taro Serigano <taroserigano@gmail.com>
2024-04-01 17:24:49 -0500

    Update cache.ts

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -2,9 +2,11 @@ import { unstable_cache as nextCache } from "next/cache"
 import { cache as reactCache } from "react"
 
 type Callback = (...args: any[]) => Promise<any>
+
 export function cache<T extends Callback>(
   cb: T,
   keyParts: string[],
+  // how frequently refetching the cache 
   options: { revalidate?: number | false; tags?: string[] } = {}
 ) {
   return nextCache(reactCache(cb), keyParts, options)
